82efee61a0ad02afcf9ae52309b53a6d87cc3ea4
Erin Anderson <e.anderson1226@gmail.com>
2020-12-01 15:52:52 -0500

    commit tree 48a7ebeefddcc99c1951d7b1fd7647833a218259

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
-class ChangeDatatypeForBirthday < ActiveRecord::Migration[5.2]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
     def change
-        change_column(table_name, column_name, type)
+        change_column :students, :birthdate, :datetime
     end
 end
